ba9d4326a38c7617112588eb24a2f3dc6cf047bc
Jesus Suarez Ardid <jsuarezardid@csc.com>
2018-05-29 15:01:56 +1000

    Add resolver for movies list

--- src/graphql/resolvers/Query.js
+++ src/graphql/resolvers/Query.js
@@ -9,11 +9,36 @@ function movie(parent, args, context, info) {
       } else {
         return results[0];
       }
-    })
+    });
 }
 
 function movies(parent, args, context, info) {
-  return moviesArray;
+  const limit = args.limit || 0;
+  const offset = args.offset || 0;
+  const query =
+    (args.title && {
+      $and: args.title
+        .replace(/,/g, " ")
+        .trim()
+        .replace(/a|á|A|Á/g, "[aáAÁ]")
+        .replace(/e|é|E|É/g, "[eéEÉ]")
+        .replace(/i|í|I|Í/g, "[iíIÍ]")
+        .replace(/o|ó|O|Ó/g, "[oóOÓ]")
+        .replace(/u|ú|U|Ú/g, "[uúUÚ]")
+        .split(" ")
+        .map(word => ({ tmdb_title: { $regex: new RegExp(word, "i") } }))
+    }) ||
+    {};
+  const sortBy = (args.sortBy && { [args.sortBy]: "desc" }) || {};
+
+  return MovieModel.find(query)
+    .sort(sortBy)
+    .skip(offset)
+    .limit(limit)
+    .exec()
+    .then(results => {
+      return results;
+    });
 }
 
 module.exports = {
